4980b575907cd980801627727992f9b63cef7b32
cenkerkumlucali <cenkerkumlucali0@gmail.com>
2021-03-12 16:25:34 +0300

    12.03.2021

--- src/app/component/car/car.component.ts
+++ src/app/component/car/car.component.ts
@@ -10,6 +10,7 @@ import { CarService } from 'src/app/services/car.service';
 export class CarComponent implements OnInit {
 cars:Car[]=[]
 dataLoaded=false;
+isDisplay=false;
   constructor(private carService:CarService) { }
 
   ngOnInit(): void {
