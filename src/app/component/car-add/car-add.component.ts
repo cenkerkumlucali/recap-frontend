d144cfb4bf532d8f4902b0bc19323aa4a12ee68a
Cenker <cenkerkumlucali0@gmail.com>
2021-04-04 18:10:29 +0300

    update

--- src/app/component/car-add/car-add.component.ts
+++ src/app/component/car-add/car-add.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup,FormBuilder,FormControl,Validators} from "@angular/forms"
 import { ToastrService } from 'ngx-toastr';
+import { Brand } from 'src/app/models/brand';
+import { Color } from 'src/app/models/color';
+import { BrandService } from 'src/app/services/brand.service';
 import { CarImageService } from 'src/app/services/car-image.service';
 import { CarService } from 'src/app/services/car.service';
+import { ColorService } from 'src/app/services/colors.service';
 @Component({
   selector: 'app-car-add',
   templateUrl: './car-add.component.html',
@@ -11,14 +15,20 @@ import { CarService } from 'src/app/services/car.service';
 export class CarAddComponent implements OnInit {
 
   carAddForm:FormGroup
+  colors:Color[]=[]
+  brands:Brand[]=[]
 
   constructor(private formBuilder:FormBuilder,
     private toastrService:ToastrService,
     private carService:CarService,
-    private carImageService:CarImageService) { }
+    private carImageService:CarImageService,
+    private brandService:BrandService,
+    private colorService:ColorService) { }
 
   ngOnInit(): void {
     this.createCarAddForm()
+    this.getBrand()
+    this.getColor()
   }
 
   createCarAddForm(){
@@ -28,6 +38,7 @@ export class CarAddComponent implements OnInit {
     modelYear:["",Validators.required],
     dailyPrice:["",Validators.required],
     description:["",Validators.required],
+    minFindeksScore:["",Validators.required],
     })
     }
     add(){
@@ -51,5 +62,15 @@ export class CarAddComponent implements OnInit {
       }else{
         this.toastrService.error("Formunuz eksik","Dikkat")
       }
+    }
+    getBrand(){
+      this.brandService.getBrand().subscribe(response=>{
+        this.brands = response.data
+      })
+    }
+    getColor(){
+      this.colorService.getColors().subscribe(response=>{
+        this.colors = response.data
+      })
     }
 }
